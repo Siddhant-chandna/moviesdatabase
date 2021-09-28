297835e27fedf14a6aa8825599d4a20d03ebc4d0
Siddhant-chandna <87367405+Siddhant-chandna@users.noreply.github.com>
2021-09-29 00:03:07 +0530

    Movie database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './Components/Navbar';
+import Search from './Components/Search';
+import Results from './Components/Results';
+import Popup from './Components/Popup'
+import React, { useState } from 'react';
+import axios from 'axios';
 function App() {
+  const apiurl="http://www.omdbapi.com/?apikey=dfe6d885";
+  // http://www.omdbapi.com/?apikey=dfe6d885                 http://www.omdbapi.com/?i=tt3896198&apikey=70011729
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
+  const search=(e)=>{
+  if(e.key==="Enter"){
+    axios(apiurl+"&s="+state.s).then(({ data })=>{
+      let results=data.Search;
+
+      setState(prevState =>{
+        return{ ...prevState,results: results}
+      })
+    });
+  }
+  }
+
+  const handleInput=(e)=> {
+    let s=e.target.value;
+    setState(prevState=>{
+      return{...prevState,s:s}
+    });
+  }
+  
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+  const [mode, setmode] = useState('light');
+
+  const togglemode= ()=>{
+   if(mode==='dark'){
+    setmode('light');
+    document.body.style.backgroundColor='white';
+   }
+   else{
+     setmode('dark');
+     document.body.style.backgroundColor='#1a0343';
+   }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className={(typeof state.selected.Title !== "undefined") ? 'app nobackground':'app background' }>
+    <>    
+   
+      <Navbar title="Movies DataBase" mode={mode} togglemode={togglemode}/>
+     <div className="App">
+       <main>
+         <Search handleInput={handleInput} search={search}/>
+         <Results results={state.results}  openPopup={openPopup}/>
+         {(typeof state.selected.Title !== "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+       </main>
+     </div>
+     
+    </>
+  //  </div>
   );
 }
 
