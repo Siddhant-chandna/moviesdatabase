c55ec84bff96aaa886468f521755fdf7dde65145
Siddhant-chandna <87367405+Siddhant-chandna@users.noreply.github.com>
2021-09-29 08:16:11 +0530

    Added more info

--- src/Components/Popup.js
+++ src/Components/Popup.js
@@ -5,11 +5,17 @@ function Popup({ selected, closePopup }) {
 		<section className="popup">
 			<div className="content">
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
-				<p className="rating">Rating: {selected.imdbRating}</p>
-				<strong>RunTime : {selected.Runtime}</strong>
+				<p className="rating">Rating: {selected.imdbRating} <br />
+				<strong>RunTime : {selected.Runtime}</strong> <br />
+				Language : {selected.Language} </p>
 				<div className="plot">
 					<img src={selected.Poster} alt=" " />
-					<p>Directed by : {selected.Director}</p>
+					<p>Actors : {selected.Actors}
+					<br />
+						Directed by : {selected.Director}
+					<br />
+					  <i> Genre : {selected.Genre}</i>
+					</p>
 					<p>{selected.Plot}</p>
 				</div>
 				<button className="close" onClick={closePopup}>Close</button>
